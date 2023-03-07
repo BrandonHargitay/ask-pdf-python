f95d9693ab6f0bc63fb2c742185d68665c1976bf
Brandon Hargitay <93677930+BrandonHargitay@users.noreply.github.com>
2023-03-06 20:26:18 -0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,231 @@
+import PyPDF2
+import os
+import openai
+
+
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+# Open the PDF file in binary mode
+with open('AppleTest.pdf', 'rb') as pdf_file:
+    # Create a PDF reader object
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
+
+    # Create a new file for writing the extracted text
+    with open('Processed_pdf.txt', 'w') as text_file:
+        # Loop over each page in the PDF file
+        for page_num in range(len(pdf_reader.pages)):
+            # Extract the text from the page
+            text = pdf_reader.pages[page_num].extract_text()
+
+            # Write the text to the output file
+            text_file.write(text)
+
+
+
+def remove_newlines(serie):
+    serie = serie.str.replace('\n', ' ')
+    serie = serie.str.replace('\\n', ' ')
+    serie = serie.str.replace('  ', ' ')
+    serie = serie.str.replace('  ', ' ')
+    return serie
+
+
+import pandas as pd
+
+# Create a list to store the text files
+texts=[]
+
+# Get all the text files in the text directory
+
+
+# Open the file and read the text
+with open("Processed_pdf.txt") as f:
+    text = f.read()
+
+texts.append(text)
+
+# Create a dataframe from the list of texts
+df = pd.DataFrame(texts, columns = ['text'])
+
+# Set the text column to be the raw text with the newlines removed
+df['text'] = remove_newlines(df.text)
+df.to_csv('processed/scraped.csv')
+df.head()
+
+
+
+import tiktoken
+
+# Load the cl100k_base tokenizer which is designed to work with the ada-002 model
+tokenizer = tiktoken.get_encoding("cl100k_base")
+
+df = pd.read_csv('processed/scraped.csv', index_col=0)
+df.columns = ['text']
+
+# Tokenize the text and save the number of tokens to a new column
+df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
+
+# Visualize the distribution of the number of tokens per row using a histogram
+df.n_tokens.hist()
+
+
+
+max_tokens = 500
+
+
+# Function to split the text into chunks of a maximum number of tokens
+def split_into_many(text, max_tokens=max_tokens):
+    # Split the text into sentences
+    sentences = text.split('. ')
+
+    # Get the number of tokens for each sentence
+    n_tokens = [len(tokenizer.encode(" " + sentence)) for sentence in sentences]
+
+    chunks = []
+    tokens_so_far = 0
+    chunk = []
+
+    # Loop through the sentences and tokens joined together in a tuple
+    for sentence, token in zip(sentences, n_tokens):
+
+        # If the number of tokens so far plus the number of tokens in the current sentence is greater
+        # than the max number of tokens, then add the chunk to the list of chunks and reset
+        # the chunk and tokens so far
+        if tokens_so_far + token > max_tokens:
+            chunks.append(". ".join(chunk) + ".")
+            chunk = []
+            tokens_so_far = 0
+
+        # If the number of tokens in the current sentence is greater than the max number of
+        # tokens, go to the next sentence
+        if token > max_tokens:
+            continue
+
+        # Otherwise, add the sentence to the chunk and add the number of tokens to the total
+        chunk.append(sentence)
+        tokens_so_far += token + 1
+
+    return chunks
+
+
+shortened = []
+
+# Loop through the dataframe
+for row in df.iterrows():
+
+    # If the text is None, go to the next row
+    if row[1]['text'] is None:
+        continue
+
+    # If the number of tokens is greater than the max number of tokens, split the text into chunks
+    if row[1]['n_tokens'] > max_tokens:
+        shortened += split_into_many(row[1]['text'])
+
+    # Otherwise, add the text to the list of shortened texts
+    else:
+
+        shortened.append(row[1]['text'])
+
+
+
+df = pd.DataFrame(shortened, columns=['text'])
+df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
+df.n_tokens.hist()
+
+
+import openai
+
+df['embeddings'] = df.text.apply(lambda x: openai.Embedding.create(input=x, engine='text-embedding-ada-002')['data'][0]['embedding'])
+
+df.to_csv('processed/embeddings.csv')
+df.head()
+
+
+
+import numpy as np
+from openai.embeddings_utils import distances_from_embeddings
+
+df=pd.read_csv('processed/embeddings.csv', index_col=0)
+df['embeddings'] = df['embeddings'].apply(eval).apply(np.array)
+df.head()
+
+
+def create_context(
+        question, df, max_len=1800, size="ada"
+):
+    """
+    Create a context for a question by finding the most similar context from the dataframe
+    """
+
+    # Get the embeddings for the question
+    q_embeddings = openai.Embedding.create(input=question, engine='text-embedding-ada-002')['data'][0]['embedding']
+
+    # Get the distances from the embeddings
+    df['distances'] = distances_from_embeddings(q_embeddings, df['embeddings'].values, distance_metric='cosine')
+
+
+    returns = []
+    cur_len = 0
+
+    # Sort by distance and add the text to the context until the context is too long
+    for i, row in df.sort_values('distances', ascending=True).iterrows():
+
+        # Add the length of the text to the current length
+        cur_len += row['n_tokens'] + 4
+
+        # If the context is too long, break
+        if cur_len > max_len:
+            break
+
+        # Else add it to the text that is being returned
+        returns.append(row["text"])
+
+    # Return the context
+    return "\n\n###\n\n".join(returns)
+
+def answer_question(
+    df,
+    question,
+    model="text-davinci-003",
+    max_len=1800,
+    size="ada",
+    debug=False,
+    max_tokens=150,
+    stop_sequence=None
+):
+    """
+    Answer a question based on the most similar context from the dataframe texts
+    """
+    context = create_context(
+        question,
+        df,
+        max_len=max_len,
+        size=size,
+    )
+    # If debug, print the raw model response
+    if debug:
+        print("Context:\n" + context)
+        print("\n\n")
+
+    try:
+        # Create a completions using the question and context
+        response = openai.Completion.create(
+            prompt=f"Answer the question truthfully based on the text below. Give details from the text to back up your answer. If you can not find the answer within the context say I don't know\"\n\nContext: {context}\n\n---\n\nQuestion: {question}\nAnswer:",
+            temperature=0,
+            max_tokens=max_tokens,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=stop_sequence,
+            model=model,
+        )
+        return response["choices"][0]["text"].strip()
+    except Exception as e:
+        print(e)
+        return ""
+
+
+_question = "What is the document about"
+_question2 = "What is the name of the current pope?"
+print("Answer: " + answer_question(df, _question, debug=False))
+print("Answer: " + answer_question(df, _question2, debug=False))
